570f3b460061ba1a4afea40d136ca67a11c4478b
crheller <charlieheller95@gmail.com>
2018-07-17 15:12:02 -0700

    tweaks to beta_comp_from_folder ploting

--- nems_lbhb/pupil_behavior_scripts/pupil_behavior_plot_demo.py
+++ nems_lbhb/pupil_behavior_scripts/pupil_behavior_plot_demo.py
@@ -13,8 +13,10 @@ click event on scatter plot will load png for that cell
 
 
 import nems_lbhb.stateplots as sp
+import pandas as pd
 
-results_path = '/auto/users/svd/projects/pupil-behavior/pb_AC_307_psth_basic-nf'
+results_path = '/auto/users/svd/projects/pupil-behavior/pb_AC_301_psth.fs20_psthfr_sdexp.S_jk.nf10-init.st-basic'
+df = pd.read_csv(results_path+'/results.csv')
 beta1 = 'r_pup'
 beta2 = 'r_beh'
 n1 = 'pupil'
@@ -22,6 +24,15 @@ n2 = 'active'
 hist_range = [-0.05, .1]
 title = 'unique pred'
 
+
+cellids = ['BRT036b-38-1']
+highlight = []
+for c in df['cellid']:
+    if c in cellids:
+        highlight.append(1)
+    else:
+        highlight.append(0)
+
 fh = sp.beta_comp_from_folder(beta1=beta1, beta2=beta2, n1=n1, n2=n2, title=title,
-                              hist_range=hist_range, folder=results_path)
+                              hist_range=hist_range, folder=results_path, highlight=highlight)
 
