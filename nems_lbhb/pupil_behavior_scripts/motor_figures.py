d3a699b8286346c0157a8be645615b79991d353b
Daniela Saderi <daniela.saderi@gmail.com>
2018-08-27 14:48:20 -0700

    change example fig in motor example script

---
+++ nems_lbhb/pupil_behavior_scripts/motor_figures.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 22 17:59:33 2018
+
+@author: daniela
+"""
+import nems_db.xform_wrappers as nw
+
+cellid="BRT036b-06-1"
+modelname="psth.fs20.pup-ld-st.pup.beh-evs.tar.lic0_fir.Nx40-lvl.1-stategain.3_jk.nf10-init.st-basic"
+batch=301
+nw.quick_inspect(cellid, batch, modelname)
