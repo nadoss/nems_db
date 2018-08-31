33f76936eb255d79d779c915e9ff70295125e27c
Stephen David <stephen.v.david@gmail.com>
2018-08-31 09:02:02 -0700

    moved some preprocssing keywords from lbhb back to main nems repo

--- nems_lbhb/plugins/lbhb_preprocessors.py
+++ nems_lbhb/plugins/lbhb_preprocessors.py
@@ -161,30 +161,5 @@ def psthfr(load_key, recording_uri):
     return xfspec
 
 
-# TODO: Maybe can keep splitep and avgep as one thing?
-#       Would they ever be done separately?
-def splitep(kw):
-    ops = kw.split('.')[1:]
-    epoch_regex = '^STIM' if not ops else ops[0]
-    xfspec = [['nems.xforms.split_by_occurrence_counts',
-               {'epoch_regex': epoch_regex}]]
-    return xfspec
-
-
-def avgep(kw):
-    ops = kw.split('.')[1:]
-    epoch_regex = '^STIM' if not ops else ops[0]
-    return [['nems.xforms.average_away_stim_occurrences',
-             {'epoch_regex': epoch_regex}]]
-
-
-def sev(kw):
-    ops = kw.split('.')[1:]
-    epoch_regex = '^STIM' if not ops else ops[0]
-    xfspec = [['nems.xforms.split_by_occurrence_counts',
-               {'epoch_regex': epoch_regex}],
-        ['nems.xforms.average_away_stim_occurrences',
-         {'epoch_regex': epoch_regex}]]
-    return xfspec
 
 
