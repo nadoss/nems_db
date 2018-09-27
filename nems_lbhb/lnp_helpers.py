0ad21b5c24e038bb0d05c3ada194c86ab27daad4
Jacob <jacob.pennington@ohsu.edu>
2018-09-27 16:12:17 -0700

    more updates to lnp metric

--- nems_lbhb/lnp_helpers.py
+++ nems_lbhb/lnp_helpers.py
@@ -29,36 +29,57 @@ def lnp_basic(modelspecs, est, max_iter=1000, tolerance=1e-7,
     return {'modelspecs': modelspecs}
 
 
-def _lnp_metric(data, rate_name, spikes_name):
+def _lnp_metric(data, pred_name='pred', resp_name='resp'):
+    # Translate SVD lab kwargs to be more readable for this model
+    rate_name = pred_name
+    spikes_name = resp_name
+
     # NOTE : don't do the averaging step when fitting this model
     # NOTE : 200hz (or maybe higher?) sampling encouraged to get
     #        at most 1 spike per bin as much as possible.
 
     # For stephen's lab: rate_name usually 'pred'
-    rate_vector = data[rate_name]
-    spike_train = data[spikes_name]
+    rate_vector = data[rate_name].as_continuous().flatten()
+    spike_signal = data[spikes_name]
+    fs = spike_signal.fs
+    spike_train = spike_signal.as_continuous()
     spikes = np.argwhere(spike_train).flatten()
 
-    # negative logikelihood: (1-integral of mu dt)(mu dt) (product for all spikes)
-    # pseudocode:
+    # negative logikelihood:
+    # (1-integral of mu dt)(mu dt) (product for all spikes)
 
     stim_errors = []
-    for stim in _stack_reps(spike_train):
-        trial_errors = []
+    stim_dict = _stack_reps(spike_signal)
 
-        for trial in stim:
+    # TODO: what to set initial to? keep it as 0? random? ISI-based?
+    integral = integrate.cumtrapz(rate_vector, initial=0).flatten()
+
+    for stim in stim_dict:
+        rep_errors = []
+
+        for rep in stim_dict[stim]:
+            start_seconds, end_seconds = rep
+            start_bin = round(start_seconds*fs)
+            end_bin = round(end_seconds*fs)
+
             loglikes = []
-            t0 = 0
-            # TODO: what to set initial to? keep it as 0? random? ISI-based?
-            integral = integrate.cumtrapz(rate_vector, initial=0)
-            for st in spikes:
-                loglike = np.log((integral[st] - integral[t0])*(rate_vector[st]))
+            t0 = start_bin
+
+            for st in spikes[start_bin: end_bin]:
+                try:
+                    loglike = np.log(
+                            (integral[st] - integral[t0])*(rate_vector[st])
+                            )
+                except IndexError:
+                    raise IndexError("Error when indexing integral for: "
+                                     "stim: %s, rep: (%f, %f)"
+                                     % (stim, start_seconds, end_seconds))
                 loglikes.append(loglike)
                 t0 = st
 
-            trial_errors.append(sum(loglikes)*-1)
+            rep_errors.append(sum(loglikes)*-1)
 
-        stim_errors.append(np.nanmean(trial_errors))
+        stim_errors.append(np.nanmean(rep_errors))
 
     error = np.nanmean(stim_errors)
 
@@ -84,3 +105,25 @@ def _lnp_metric(data, rate_name, spikes_name):
     # supposed to recover spikes of response
 
     return error
+
+
+def _stack_reps(spike_train, ep='^STIM_'):
+    stim_dict = {}
+    epochs = spike_train.epochs
+
+    for name in sorted(set(nems.epoch.epoch_names_matching(epochs, ep))):
+
+        rows = epochs[epochs.name == name]
+        reps = zip(
+                rows['name'].values.tolist(),
+                rows['start'].values.tolist(),
+                rows['end'].values.tolist()
+                )
+
+        for name, start, stop in reps:
+            if name in stim_dict:
+                stim_dict[name].append((start, stop))
+            else:
+                stim_dict[name] = [(start, stop)]
+
+    return stim_dict
