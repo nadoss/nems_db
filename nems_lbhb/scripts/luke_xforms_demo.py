1f94a3469fa7a7f7577a3b6e2fa9c65e62ca69db
Luke Shaheen <LukeShaheen@gmail.com>
2019-01-15 17:20:24 -0800

    modernize

--- nems_lbhb/scripts/luke_xforms_demo.py
+++ nems_lbhb/scripts/luke_xforms_demo.py
@@ -5,17 +5,18 @@ Created on Wed Mar  7 15:30:15 2018
 
 @author: svd
 """
-
-import matplotlib.pyplot as plt
-import numpy as np
 import os
-import io
-
+os.environ['OPENBLAS_CORETYPE'] = 'sandybridge'
+os.environ['OPENBLAS_VERBOSE'] = '2'
 #import nems.recording
 import nems.modelspec as ms
 import nems.xforms as xforms
 import nems.xform_helper as xhelp
 import nems.utils
+
+import matplotlib.pyplot as plt
+import numpy as np
+import io
 
 #import nems_db.baphy as nb
 import nems_db.db as nd
@@ -24,6 +25,7 @@ from nems_lbhb.old_xforms.xform_wrappers import generate_recording_uri as ogru
 import nems_lbhb.old_xforms.xform_helper as oxfh
 import logging
 
+logging.basicConfig(level='DEBUG')
 log = logging.getLogger(__name__)
 
 
@@ -36,7 +38,11 @@ from nems.plugins import default_initializers
 from nems.plugins import default_fitters
 from nems.gui.recording_browser import browse_recording, browse_context
 
-
+def _xform_exists(xfspec, xform_fn):
+    for xf in xfspec:
+        if xf[0] == xform_fn:
+            return True
+        
 batch = 289
 cellid ='BRT026c-46-1'
 modelname = "ozgf.fs100.ch18-ld-sev_dlog.f-wc.18x2.g-fir.2x15-lvl.1_init-basic"
@@ -47,7 +53,24 @@ cellid="TAR010c-06-1"
 modelname = "psth.fs20.pup-ld-st.pup0.beh0-ref-psthfr_stategain.S_jk.nf10-basic"
 modelname = "psth.fs20.pup-ld-st.pup.beh-ref-psthfr_stategain.S_jk.nf10-basic"
 
-autoPlot = True
+batch=306
+cellid='fre192b-03-1_6236-1701'
+cellid='fre196b-35-1_1417-1233'
+modelname='env.fs100-SPOld-sev_dlog-fir.2x15-lvl.1-dexp.1_init.nl1-basic-SPOpf'
+modelname='env.fs100-SPOld-sev_dlog-fir.2x15-lvl.1-dexp.1_init.nl2-basic-SPOpf.SDB'
+modelname='env.fs100-SPOld-sev_dlog-wc.2x2.c-fir.2x15-lvl.1-dexp.1_init.nl1-basic-SPOpf.SDB'
+modelname='env.fs100-SPOld-subset.A+B-sev_dlog-fir.2x15-lvl.1-dexp.1_init.nl1-basic-SPOpf.SDB'
+modelname='SDB-env.fs100-SPOld-stSPO-sev_dlog-stategain.3x2.g-fir.2x15-lvl.1-dexp.1_init-basic-SPOpf'
+#Shuffled ver here
+
+#modelname='env.fs100-SPOld-sev_dlog-fir.2x15-lvl.1-dexp.1_init'
+#modelname='env.fs100-SPOld-sev_dlog-fir.2x15-lvl.1-dexp.1_init.nl2-basic-SPOpf'
+
+autoPred = True
+autoStats = True
+autoPlot = False
+
+saveToFile=False
 saveInDB = False
 browse_results = False
 
@@ -66,20 +89,12 @@ meta = {'batch': batch, 'cellid': cellid, 'modelname': modelname,
         'githash': os.environ.get('CODEHASH', ''),
         'recording': loadkey}
 
-recording_uri = nw.generate_recording_uri(cellid, batch, loadkey)
-# code from
-# xfspec = xhelp.generate_xforms_spec(recording_uri, modelname, meta)
-"""
-{'stim': 0, 'chancount': 0, 'pupil': 1, 'rasterfs': 20, 'rawid': None, 'cellid': 'BRT026c-15-1', 'pupil_median': 0, 'pertrial': 0, 'pupil_deblink': 1, 'stimfmt': 'parm', 'runclass': None, 'includeprestim': 1, 'batch': 307}
-{'stimfmt': 'parm', 'chancount': 0, 'pupil': 1, 'rasterfs': 20, 'rawid': None, 'cellid': 'BRT026c-15-1', 'pupil_median': 0, 'pertrial': 0, 'pupil_deblink': 1, 'stim': 0, 'runclass': None, 'includeprestim': 1, 'batch': 307}
-"""
-log.info('Initializing modelspec(s) for recording/model {0}/{1}...'
-         .format(recording_uri, modelname))
-
-# parse modelname and assemble xfspecs for loader and fitter
 load_keywords, model_keywords, fit_keywords = modelname.split("_")
 
-xforms_kwargs = {'cellid': cellid, 'batch': int(batch)}
+# xforms_kwargs = {'cellid': cellid, 'batch': int(batch)}
+xforms_kwargs = {}
+xforms_init_context = {'cellid': cellid, 'batch': int(batch),
+                       'meta': meta, 'keywordstring': model_keywords}
 xforms_lib = KeywordRegistry(**xforms_kwargs)
 xforms_lib.register_modules([default_loaders, default_fitters,
                              default_initializers])
@@ -93,25 +108,34 @@ keyword_lib.register_plugins(get_setting('KEYWORD_PLUGINS'))
 # to run through (like a packaged-up script)
 xfspec = []
 
+# 0) set up initial context
+xfspec.append(['nems.xforms.init_context', xforms_init_context])
+
 # 1) Load the data
 xfspec.extend(xhelp._parse_kw_string(load_keywords, xforms_lib))
 
 # 2) generate a modelspec
-xfspec.append(['nems.xforms.init_from_keywords',
-               {'keywordstring': model_keywords, 'meta': meta,
-                'registry': keyword_lib}])
+xfspec.append(['nems.xforms.init_from_keywords', {'registry': keyword_lib}])
+#xfspec.append(['nems.xforms.init_from_keywords', {}])
 
 # 3) fit the data
 xfspec.extend(xhelp._parse_kw_string(fit_keywords, xforms_lib))
 
-# 4) add some performance statistics
-xfspec.append(['nems.xforms.predict', {}])
-xfspec.append(['nems.xforms.add_summary_statistics', {}])
+# 4) generate a prediction (optional)
+if autoPred:
+    if not _xform_exists(xfspec, 'nems.xforms.predict'):
+        xfspec.append(['nems.xforms.predict', {}])
+
+# 5) add some performance statistics (optional)
+if autoStats:
+    if not _xform_exists(xfspec, 'nems.xforms.add_summary_statistics'):
+        xfspec.append(['nems.xforms.add_summary_statistics', {}])
 
 # 5) generate plots
 if autoPlot:
-    log.info('Generating summary plot...')
-    xfspec.append(['nems.xforms.plot_summary', {}])
+    if not _xform_exists(xfspec, 'nems.xforms.plot_summary'):
+        log.info('Generating summary plot...')
+        xfspec.append(['nems.xforms.plot_summary', {}])
 
 # Create a log stream set to the debug level; add it as a root log handler
 log_stream = io.StringIO()
@@ -127,6 +151,32 @@ ctx = {}
 for xfa in xfspec:
     ctx = xforms.evaluate_step(xfa, ctx)
 
+
+if False:
+    Ipred = np.where([xf[0]=='nems.xforms.predict' for xf in xfspec])[0][0]
+    Ifit_init = np.where([xf[0]=='nems.xforms.fit_basic_init' for xf in xfspec])[0][0]
+    Ifit = np.where([xf[0]=='nems.xforms.fit_basic' for xf in xfspec])[0][0]
+    Isbc = np.where([xf[0]=='nems_lbhb.postprocessing.add_summary_statistics_by_condition' for xf in xfspec])[0][0]
+    Ipav = np.where([xf[0]=='nems_lbhb.SPO_helpers.plot_all_vals_' for xf in xfspec])[0][0]
+    ctx = {}
+    for xfa in xfspec[:Ifit_init+1]:
+        ctx = xforms.evaluate_step(xfa, ctx)
+    ctxI=ctx.copy()
+    
+    ctx=ctxI.copy()
+    ctx['modelspecs'][0] = init_dexp(ctx['est'], ctx['modelspecs'][0])
+    #ctx['modelspecs'][0][3]['phi']['kappa'][0]=5
+    #ctx['modelspecs'][0][3]['phi']['amplitude'][0]=.09
+    xfi=list(range(Ifit_init+1,len(xfspec)))
+    xfi.remove(Ifit); 
+    xfi.remove(Isbc); xfi.remove(Ipav)
+    #xfi.insert(0,Ifit_init)
+    for xfa in [xfspec[i] for i in xfi]:
+        ctx = xforms.evaluate_step(xfa, ctx)
+    ctx['modelspecs'][0][0]['meta']['r_test']
+    
+    ctxF=ctx.copy()
+
 # Close the log, remove the handler, and add the 'log' string to context
 log.info('Done (re-)evaluating xforms.')
 ch.close()
@@ -135,26 +185,27 @@ rootlogger.removeFilter(ch)
 log_xf = log_stream.getvalue()
 
 # save some extra metadata
-modelspecs = ctx['modelspecs']
-
+modelspec = ctx['modelspec']
+#
 destination = '/auto/data/nems_db/results/{0}/{1}/{2}/'.format(
-        batch, cellid, ms.get_modelspec_longname(modelspecs[0]))
-modelspecs[0][0]['meta']['modelpath'] = destination
-modelspecs[0][0]['meta']['figurefile'] = destination+'figure.0000.png'
+        batch, cellid, ms.get_modelspec_longname(modelspec))
+modelspec['meta']['modelpath'] = destination
+modelspec['meta']['figurefile'] = destination+'figure.0000.png'
 
 # save results
-# log.info('Saving modelspec(s) to {0} ...'.format(destination))
-# xforms.save_analysis(destination,
-#                      recording=ctx['rec'],
-#                      modelspecs=modelspecs,
-#                      xfspec=xfspec,
-#                      figures=ctx['figures'],
-#                      log=log_xf)
+if saveToFile:
+    log.info('Saving modelspec(s) to {0} ...'.format(destination))
+    xforms.save_analysis(destination,
+                      recording=ctx['rec'],
+                      modelspec=modelspec,
+                      xfspec=xfspec,
+                      figures=ctx['figures'],
+                      log=log_xf)
 
 # save in database as well
 if saveInDB:
     # TODO : db results finalized?
-    nd.update_results_table(modelspecs[0])
+    nd.update_results_table(modelspec)
 
 if browse_results:
     aw = browse_context(ctx, signals=['stim', 'pred', 'resp'])
